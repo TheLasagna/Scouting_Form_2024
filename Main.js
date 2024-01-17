3dcbe684f7eec5b867c0f6f99afaecf25daa7b7c
TheLasagna <150753015+TheLasagna@users.noreply.github.com>
2024-01-17 16:41:38 +0000

    lol

--- Main.js
+++ Main.js
@@ -42,5 +42,35 @@ document.getElementById("counter-value3").innerHTML = counter3
 }
 
 function submit(){
-    ///Nikhil, need halp. How to create QR? I made buttons work though :D
-}
+    let qrContentInput = document.getElementById("qr-content");
+    let qrGenerationForm = 
+    document.getElementById("qr-generation-form");
+    let qrCode;
+     
+    function generateQrCode(qrContent) {
+      return new QRCode("qr-code", {
+        text: qrContent,
+        width: 256,
+        height: 256,
+        colorDark: "#000000",
+        colorLight: "#ffffff",
+        correctLevel: QRCode.CorrectLevel.H,
+      });
+    }
+     
+    // Event listener for form submit event
+    qrGenerationForm.addEventListener("submit", function (event) {
+      // Prevent form submission
+      event.preventDefault();
+      let qrContent = qrContentInput.value;
+      if (qrCode == null) {
+           
+        // Generate code initially
+        qrCode = generateQrCode(qrContent);
+      } else {
+           
+        // If code already generated then make 
+        // again using same object
+        qrCode.makeCode(qrContent);
+      }
+    })}
